15e87dd4f51e097fdcd9a64043400a3a568c6d8b
Nicole Bond <nicbond22@gmail.com>
2025-04-27 12:08:29 -0400

    Imported SwiftData to all files so app stores previously saved data and "remembers" when app is completely closed and opened again

--- BirthdaysApp/ContentView.swift
+++ BirthdaysApp/ContentView.swift
@@ -6,19 +6,55 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    
+    @Query private var friends: [Friend]
+    @Environment(\.modelContext) private var context
+//    = [ Friend(name:"Tori Kelly", birthday: .now),
+//        Friend(name: "Demi Lovato", birthday: Date(timeIntervalSince1970: 0)) ]
+    
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(friends) { friend in
+                HStack {
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+            }
+            .navigationTitle("Birthdays")
         }
-        .padding()
+        .safeAreaInset(edge: .bottom) {
+            
+            VStack {
+                Text("New Birthday")
+                    .font(.headline)
+                
+                DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                    TextField("Name", text: $newName)
+                        .textFieldStyle(.roundedBorder)
+                }
+                
+                Button("Save") {
+                    let newFriend = Friend(name: newName, birthday: newBirthday)
+                    context.insert(newFriend)
+                    newName = ""
+                    newBirthday = Date.now
+                }
+                .bold()
+            }
+            .padding()
+            .background(.bar)
+        }
     }
 }
 
 #Preview {
     ContentView()
+        .modelContainer(for: Friend.self, inMemory: true)
 }
