f478b4aaf30aac5d150f0d29441e2cb29635b9e8
Nicole Bond <nicbond22@gmail.com>
2025-04-27 11:30:36 -0400

    Initial Commit

---
+++ BirthdaysApp/BirthdaysAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysAppApp.swift
+//  BirthdaysApp
+//
+//  Created by Scholar on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
